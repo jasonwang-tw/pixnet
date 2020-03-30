ca9b77530b69e344d3ce21befb6912b168d8b9ab
jason@ecodes.com.tw <jason@ecodes.com.tw>
2020-03-30 18:36:23 +0800

    更改Get 資料, 並加入可彈窗影片檔以及音檔之模組

--- js/all.js
+++ js/all.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest();
 
 // 取得資料
-xhr.open('get','https://emma.pixnet.cc/album/elements?set_id=4555593&user=jiney&format=json',true);
+xhr.open('get','https://emma.pixnet.cc/album/elements?format=json&set_id=34260&user=emmademo&use_iframe=1',true);
 
 // 回傳資料(null)
 xhr.send(null);
@@ -11,18 +11,29 @@ xhr.onload = function () {
 
     // 字串轉換
     var str = JSON.parse(xhr.responseText);
-
     var strLen = str.elements.length;
+    var user = document.querySelector('.user');
+    var strUser = str.set.user.display_name;
+    user.textContent = strUser + ' 的相簿';
 
     // 載入資料&彈窗
     var wrapIn = '';
     for(var f=0;f<strLen;f++){
         var strThumb = str.elements[f].thumb;
         var strLarge = str.elements[f].identifier;
+        var strVideo = str.elements[f].id;
+        var strType= str.elements[f].type;
         var wrap = document.querySelector('.imgList');
-        var content = '<a data-fancybox="gallery" href="https://pic.pimg.tw/jiney/' + strLarge + '_b.jpg"><img src="' + strThumb + '"></a>'
-        wrapIn += content;
+        if(strType == 'pic'){
+           var content = '<a data-fancybox="gallery" href="https://pic.pimg.tw/emmademo/' + strLarge + '.jpg"><img src="' + strThumb + '"></a>'
+           wrapIn += content;
+        }else if(strType == 'video'){
+           var content = '<a data-fancybox="gallery" data-type="iframe" data-src="https://emmademo.pixnet.net/album/video/'+ strVideo +'" href="javascript:;"><img src="' + strThumb + '"></a>'   
+           wrapIn += content;
+        }else if(strType == 'audio'){
+           var content = '<a data-fancybox="gallery" data-type="video" data-src="https://ssl-pixnet-tv.pixfs.net/user/emmademo/'+ strLarge +'.m4a"><img src="' + strThumb + '"></a>'   
+           wrapIn += content;
+        } 
     }
     wrap.innerHTML = wrapIn;
-    
 }
